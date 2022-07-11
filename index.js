340d0d00168fce8dfe9371fc5ddf32eb8bb08eb2
Mitali2700 <201B155@juetguna.in>
2022-07-11 22:57:14 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("hello world");
+// document.querySelector("button").addEventListener("click", handleClick);
+// // If we write handleClick with paranthesis inside the .addEventListener then imidiately popup will come when we refresh our website 
+
+// function handleClick(){
+//     alert("I got clicked!");
+//}
+
+
+for(var i=0; i<document.querySelectorAll(".drum")){
+
+}
+document.querySelector("button").addEventListener("click", function(){
+    alert("I got clicked!");
+});
