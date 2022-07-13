06c36f43d785e7b61cf9583b4ab8b556b8a41218
Mitali2700 <201B155@juetguna.in>
2022-07-13 12:46:52 +0530

    adding sound

--- index.js
+++ index.js
@@ -5,10 +5,13 @@
 //     alert("I got clicked!");
 //}
 
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for(var i=0; i<document.querySelectorAll(".drum")){
+for(var i=0; i<numberOfDrumButtons; i++){
 
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        // alert("I got clicked!");
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+    });
 }
-document.querySelector("button").addEventListener("click", function(){
-    alert("I got clicked!");
-});
