c9fb4246c6e39d16c417f442c680e07cd398c5f2
Mitali2700 <201B155@juetguna.in>
2022-07-14 22:58:58 +0530

    update- sound

--- index.js
+++ index.js
@@ -36,42 +36,42 @@ function makeSound(key){
     switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            audio.play();
+            tom1.play();
             break;
         
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
-            audio.play();
+            tom2.play();
             break;
 
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3");
-            audio.play();
+            tom3.play();
             break;
 
         case "d":
             var tom4 = new Audio("sounds/tom-4.mp3");
-            audio.play();
+            tom4.play();
             break; 
 
         case "j":
             var snare = new Audio("sounds/snare.mp3");
-            audio.play();
+            snare.play();
             break;    
             
         case "k":
             var crash = new Audio("sounds/crash.mp3");
-            audio.play();
+           crash.play();
             break;
 
         case "l":
                 var kick = new Audio("sounds/kick.mp3");
-                audio.play();
+                kick.play();
                 break;
         
                 
         default: console.log(buttonInnerHTML);
-            
+            break;
 
     }
 
@@ -81,4 +81,8 @@ function buttonAnimation(currentKey){
 
     document.querySelector("."+currentKey);
     activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
 }
